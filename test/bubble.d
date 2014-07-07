e72b290057c5e11e9e18aa1c8d1e3155689e4c1a
jxwr <jxwr.cn@gmail.com>
2014-07-08 01:31:45 +0800

    indent

--- test/bubble.d
+++ test/bubble.d
@@ -1,20 +1,20 @@
 gaps = [701, 301, 132, 57, 23, 10, 4, 1]
 
 func bubble(lst) {
-     swapped = true
-     for i = lst.length() - 1; i > 0 && swapped; i-- {
-     	 swapped = false
-	 for j = 0; j < i; j++ {
-	     if lst[j] > lst[j+1] {
-	     	tmp = lst[j]
-	     	lst[j] = lst[j+1]
-	     	lst[j+1] = tmp
-	     	print(lst, "\n")
-	     	swapped = true
-	     }
-	 }
-     }
-     return lst
+	swapped = true
+	for i = lst.length() - 1; i > 0 && swapped; i-- {
+		swapped = false
+		for j = 0; j < i; j++ {
+			if lst[j] > lst[j+1] {
+				tmp = lst[j]
+				lst[j] = lst[j+1]
+				lst[j+1] = tmp
+				print(lst, "\n")
+				swapped = true
+			}
+		}
+	}
+	return lst
 }
 
 print(gaps, "\n")
